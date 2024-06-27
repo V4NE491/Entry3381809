ec09a15e8843a4cff32b321daf2144877c845992
Indel <Indel@DESKTOP-BEC38OO>
2024-06-27 14:47:55 -0600

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Entry3381809
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Entry3381809
+{
+    public partial class MainPage : ContentPage
+    {
+      
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string pasado = e.OldTextValue;
+            string nuevo = e.NewTextValue;
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            string text= ((Entry)sender).Text;
+        }
+    }
+
+}
